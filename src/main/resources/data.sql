fa36e6362db58b3c0522d5eb4932acee5da78f64
Sebastien Mimi <mimi@Sebastien-Mimis-Imac.local>
2020-06-12 14:44:04 +0400

    push latest

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO employee(id,name,status) VALUES(0,'mimi','present');
+INSERT INTO employee(id,name,status) VALUES(1,'steohanie','present');
+INSERT INTO employee(id,name,status) VALUES(2,'jean paul','present');
+INSERT INTO employee(id,name,status) VALUES(3,'ludovik','absent');
+INSERT INTO employee(id,name,status) VALUES(4,'rajeev','present');
+INSERT INTO employee(id,name,status) VALUES(5,'stephen','present');
+INSERT INTO employee(id,name,status) VALUES(6,'sabrina','absent');
+INSERT INTO employee(id,name,status) VALUES(7,'dewashish','present');
+INSERT INTO employee(id,name,status) VALUES(8,'mathew','present');
+
+COMMIT;
---
+++ src/test/java/com/example/mobimoveEmployee/MobimoveEmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mobimoveEmployee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobimoveEmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
